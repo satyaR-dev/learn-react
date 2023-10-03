e2b3b81d55e2671189c29177dcfb2b015971dbe6
Bararuchi <bararuchi1@hotmail.com>
2023-10-04 02:29:20 +0530

    Assignmemt-01 Inception

---
+++ App.js
@@ -0,0 +1,45 @@
+// How to write hellow world using React
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello World using React!!"
+);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+/**
+ *
+ * <div id="parent">
+ *        <div id="child">
+ *        <h1>I am h1 tag...</h1>
+ *        <h2>I am h2 tag...</h2>
+ * </div>
+ * <div id="child2">
+ *        <h1>I am h1 tag of child2...</h1>
+ *        <h2>I am h2 tag of child2...</h2>
+ * </div>
+ * </div>
+ *
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I am h1 tag..."),
+      React.createElement("h2", {}, "I am h2 tag..."),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I am h1 tag of child2..."),
+      React.createElement("h2", {}, "I am h2 tag of child2..."),
+    ]),
+  ] // We can add multiple tag in an array
+);
+
+// The above code is very messy so, in this case JSX came intp picture
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
